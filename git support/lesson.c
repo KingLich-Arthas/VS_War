b5b0b5b97efc4fa703d534ac2f10a42fdf13436b
KingLich-Arthas <smile44804@gmail.com>
2025-12-23 18:57:03 +0200

    Добавление в lesson функции вычисления значения b=f(x,y,z), где z=w(x,y).

--- git support/lesson.c
+++ git support/lesson.c
@@ -2,16 +2,20 @@
 #include<conio.h>
 #include<math.h>
 #include<locale.h>
+void function (float  x, float y)
+{
+	float z = (3 * x) / (pow (cos(y), 2));
+	float f = (pow(z, 2) / (y + pow(x, 3))) + sin(y / 5);
+	float b = f;
+	printf("Переменная z равна: %f\n", z);
+	printf("Функия b равна: %f\n", b);
+}
 void main(void)
 {
 	float x, y, z, f, b;
 	setlocale(LC_ALL, "rus");
 	x = 1,58;
 	y = 3,42;
-	z = (3 * x) / (pow (cos(y), 2));
-	f = (pow(z, 2) / (y + pow(x, 3))) + sin(y / 5);
-	b = f;
-	printf("Переменная z равна: %f\n", z);
-	printf("Функия b равна: %f\n", b);
+	function(x, y);
 	_getch();
 }
